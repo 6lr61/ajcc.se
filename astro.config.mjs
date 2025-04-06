df80f0b483dee960dc2c17d4a6d548c4ffbe054f
Adrian <107351903+6lr61@users.noreply.github.com>
2025-04-06 16:13:30 +0200

    feat: anchor links on markdown headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/blog/post": "/blog",
     "/blog/tag": "/blog/tags",
-  }
+  },
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "after",
+          properties: { className: "anchor-link" },
+          content: {
+            type: "text",
+            value: "#",
+          },
+          group: (node, link) => {
+            return {
+              type: "element",
+              tagName: "div",
+              properties: { className: "heading-group" },
+              children: [node, link],
+            };
+          },
+        },
+      ],
+    ],
+  },
 });
